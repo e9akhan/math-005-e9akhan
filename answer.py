46bc31d9fe53136499d95795f871c86b19e8718b
Akbar Khan <akbar.khan@enine.school>
2023-12-15 17:04:38 +0530

    Styled

--- answer.py
+++ answer.py
@@ -1,2 +1,18 @@
+"""
+    Module name :- answer.py
+    Method(s) :- answer()
+"""
+
+from solver import solver
+
+
 def answer():
-    return -1
+    """
+    Find the smallest positive number which is divisible by
+    each number from 1-10
+    """
+    return solver(1, 10)
+
+
+if __name__ == "__main__":
+    print(answer())
--- solver.py
+++ solver.py
@@ -1,2 +1,36 @@
+"""
+    Module name :- solver
+    Method(s) :- solver(p, q)
+"""
+
+
 def solver(p, q):
-    return -1
+    """
+    Find the number which is divisible by each number
+    over the range.
+
+    Args:
+        p(int) :- Starting point of the range
+        q(int) :- Ending point of the range.
+
+    Return:
+        Smallest number which is divisible by each number
+        over the range.
+    """
+    number = 0
+
+    if p > q:
+        temp, p = p, q
+        q = temp
+
+    while True:
+        number += q
+        for i in range(p, q + 1):
+            if number % i != 0:
+                break
+        else:
+            return number
+
+
+if __name__ == "__main__":
+    print(solver(1, 20))
